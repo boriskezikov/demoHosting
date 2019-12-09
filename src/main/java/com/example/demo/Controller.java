2044a31c0cbfe0457221808c1a4d5044b6fd8758
Kezikov Boris <boris200898@icloud.com>
2019-12-10 01:23:22 +0300

    server impl

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/android")
 public class Controller {
 
+    private final DataSourceRoutingService service;
+
+    @Autowired
+    public Controller(DataSourceRoutingService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public String printMyString() {
-        return "Люблю полинку)))";
+    public String printMyString(@RequestParam String letter) throws Exception {
+        return service.getCityByFirstLetter(letter);
     }
 }
---
+++ src/main/java/com/example/demo/DataSourceRoutingService.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+
+@Service
+@EnableCaching
+public class DataSourceRoutingService {
+
+    private static final String GET_CITY_BY_NAME = "select name from city where name like ?";
+    private static final Random rand = new Random();
+
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public DataSourceRoutingService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @CacheEvict("city")
+    public String getCityByFirstLetter(String letter) throws Exception {
+        if (letter.length() != 1) {
+            throw new Exception("Incorrect input");
+        }
+        List<String> response = jdbcTemplate.queryForList(GET_CITY_BY_NAME, String.class, letter + '%');
+        return response.get(rand.nextInt(response.size()));
+    }
+}
